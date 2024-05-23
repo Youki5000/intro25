368035ee084dfe5ffc6bf4283b34fc8645ec2379
Youki5000 <147134665+Youki5000@users.noreply.github.com>
2024-05-23 23:20:25 +0900

    Create animation.js

---
+++ css-programming/animation.js
@@ -0,0 +1,13 @@
+let heading = document.getElementById('heading');
+let degree = 0;
+function rotateHeading() {
+  degree = degree + 6;
+  degree = degree % 360;
+  if (90 <= degree && degree < 270) {
+    heading.setAttribute('class', 'back');
+  } else {
+    heading.setAttribute('class', 'face');
+  }
+  heading.style.transform = 'rotateX(' + degree + 'deg)';
+}
+setInterval(rotateHeading, 20);
